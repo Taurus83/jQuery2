f5fa3a9b2f1b2d280aa6e35b4def6aff70dd4ce4
John Doe <john.doe@example.com>
2017-05-14 13:59:01 +0200

    jquery js carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
-$(function(){
-	var carouselList = $('#carousel ul');	//ten kod wykona się po załadowaniu DOM
-});	
-
-setInterval(changeSlide, 3000); //co 3 sek. wykona funkcję zmieniającą slajd
-
-$(changeSlides(){
-	carouselList.animate({'marginLeft':-550}, 500, moveFirstSlide);
-});	 
-
-$(moveFirstSlide(){
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last")
+$(function() {
 	
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-});
+	setInterval(changeSlides, 3000); //co 3 sek. wykona funk
 
-$('#right').on('click', function(){
-   carouselList.animate({scrollLeft: -firstItem}, 500);
-});
+	var carouselList = $('#carousel ul');	//ten kod wykona się po załadowaniu DOM
 
+	function changeSlides() {
+		carouselList.animate({'marginLeft':-550}, 500, moveFirstSlide);
+	};
 
-$('#left').on('click', function(){
-    carouselListl.animate({scrollLeft: firstItem}, 500);
-});
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last")
+	
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: 0});
+	};
+	changeSlides();
+	moveFirstSlide();
 
+	var move = 0;
+		$("#right-button").click(function(){
+    	move += 550;
+    	$("#carousel ul").animate({
+      	marginLeft: move+"px"
+    	}, 500);
+	});
 
+		$("#left-button").click(function(){
+    	move -= 550;
+    	$("#carousel ul").animate({
+      	marginLeft: move+"px"
+    	}, 500);
+	});
+
+});	
 
 
 
